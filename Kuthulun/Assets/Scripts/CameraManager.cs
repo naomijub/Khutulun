4ed14ea03c71eed7196d1b1839ed87ecd373dc5e
naomijub <jnboeira@outlook.com>
2016-12-19 18:05:20 -0300

    Camera Manager, Player Controller and bush assets

---
+++ Kuthulun/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 2.0f;
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - target.position + new Vector3(0.0f, -2.0f, 2.0f);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 targetCamPos;
+
+		if (target.position.y > 70.0f) {
+			targetCamPos = new Vector3 (target.position.x, 71.0f, target.position.z) + offset;
+			smoothing = 0.8f;
+		} else if (target.position.x > 40.0f) {
+			targetCamPos = new Vector3 (42.0f, target.position.y, target.position.z) + offset;
+			smoothing = 0.8f;
+		} else if (target.position.x < -41.0f) {
+			targetCamPos = new Vector3 (-43.0f, target.position.y, target.position.z) + offset;
+			smoothing = 0.8f;
+		} else {
+			targetCamPos = target.position + offset;
+			smoothing = 2.0f;
+		}
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
--- Kuthulun/Assets/Scripts/PlayerController.cs
+++ Kuthulun/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,44 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	Rigidbody2D rb2d;
+	Animator anim;
+	enum Dir {up, down, left, right }
+
+	public float speed = 96.0f;
+	Dir dir = Dir.down;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float y = Input.GetAxis("Vertical");
+		float x = Input.GetAxis("Horizontal");
+
+		rb2d.velocity = new Vector2 (x * speed * Time.deltaTime, y * speed * Time.deltaTime);
+
+		Animations (x, y);
+
 	}
+		
+	public void Animations(float x, float y){
+		if (y < 0 && x == 0 && dir != Dir.down) {
+			anim.SetTrigger ("walkDown");
+			dir = Dir.down;
+		} else if (y > 0 && x == 0 && dir != Dir.up) {
+			anim.SetTrigger ("walkUp");
+			dir = Dir.up;
+		} else if (x > 0 && y == 0 && dir != Dir.right) {
+			anim.SetTrigger ("walkRight");
+			dir = Dir.right;
+		} else if (x < 0 && y == 0 && dir != Dir.left) {
+			anim.SetTrigger ("walkLeft");
+			dir = Dir.left;
+		}
+	}
+
 }
